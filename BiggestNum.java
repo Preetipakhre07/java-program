30401988e231d3098882855e69720d907d18474a
Preetipakhre07 <124261713+Preetipakhre07@users.noreply.github.com>
2023-03-03 08:28:21 +0530

    Add files via upload

---
+++ BiggestNum.java
@@ -0,0 +1,22 @@
+import java.util.scanner;
+class BiggestNum
+{
+  public static void main(string[]args)
+  {
+      int a,b,Biggest;
+      scanner sc=new scanner(System.in);
+      system.out.print("Enter the First Number: ");
+      a=Sc.nextInt();
+      System.out.print("Enter the Second Number: ");
+      b=Sc.nextInt();
+
+
+    if(a>b) 
+    Biggest=a;
+else
+    Biggest=b;
+
+     System.out.println(" Enter Biggest number"= +"Biggest");
+   }
+
+}
