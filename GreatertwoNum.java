c71a1709998ee88445d0ab13423c49d4fa08e901
Preetipakhre07 <124261713+Preetipakhre07@users.noreply.github.com>
2023-03-03 08:25:59 +0530

    Add files via upload

---
+++ GreatertwoNum.java
@@ -0,0 +1,17 @@
+class GreatertwoNum
+   {
+   public static void main(String[]args)
+  {
+      int a=20,b=50;
+      if(a>b)
+{
+   System.out.println("a is greater");
+}
+      else
+  System.out.println("b is greater");
+  }
+   }
+
+
+
+
